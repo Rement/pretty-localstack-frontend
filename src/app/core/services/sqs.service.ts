e9e80ac9cb282c9b78dab8c438ea397d13276478
Andrei Kaliachka <andrei_kaliachka@epam.com>
2023-02-27 01:44:37 -0800

    Refactor sqs service

--- src/app/core/services/sqs.service.ts
+++ src/app/core/services/sqs.service.ts
@@ -11,26 +11,33 @@ import { MessageModel } from '../../shared/models/message.model';
 })
 export class SqsService {
 
+  public sqsBaseUrl: string;
+
   constructor(private http: HttpClient) {
+    this.sqsBaseUrl = '/sqs';
   }
 
   getQueueList(): Observable<string[]> {
-    return this.http.get<string[]>(`/queues`);
+    return this.http.get<string[]>(this.sqsBaseUrl + `/queues`);
   }
 
   getQueueInfo(queueUrl: string): Observable<QueueInfo> {
-    return this.http.post<any>(`/queue`, queueUrl)
+    const encodedUrl: string = encodeURIComponent(queueUrl);
+    let params = new HttpParams();
+    params = params.append('queryUrl', encodedUrl);
+    return this.http.get<any>(this.sqsBaseUrl + `/queue`, {params: params})
   }
 
   getMessages(queueUrl: string): Observable<MessageModel[]> {
-    return this.http.post<any>(`/receive`, queueUrl)
+    const encodedUrl: string = encodeURIComponent(queueUrl);
+    let params = new HttpParams();
+    params = params.append('queryUrl', encodedUrl);
+    return this.http.get<any>(this.sqsBaseUrl + `/receive`, {params: params})
   }
 
   sendMessage(queueUrl: string, message: string): Observable<SendMessageResponseModel> {
     const messageRequest: MessageRequest = new MessageRequest(message, queueUrl);
-    return this.http.post<any>(`/send`, messageRequest);
+    return this.http.post<any>(this.sqsBaseUrl + `/send`, messageRequest);
   }
-
-
 
 }
