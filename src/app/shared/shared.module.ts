a0ea2f761478a343aaf90d00e79977130da6ab4c
Andrei Kaliachka <andrei_kaliachka@epam.com>
2023-02-27 20:22:55 -0800

    Rewrite header and remove nav-bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,11 +10,9 @@ import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     HeaderComponent,
-    SideNavComponent
   ],
   exports: [
     HeaderComponent,
-    SideNavComponent
   ],
   imports: [
     CommonModule,
